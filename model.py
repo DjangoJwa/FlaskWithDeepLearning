731468f87a0888737f38317af25148b9cff816a5
Xsolute Lim <Xsolute01@gmail.com>
2018-11-06 17:24:43 +0900

    MOD: 모델 구성
    CIFAR10 구별하도록 간단한 CNN 모델 구성

--- model.py
+++ model.py
@@ -1,4 +1,26 @@
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from dataset import loadCIFAR10
+
+(x_train, y_train), (x_test, y_test) = loadCIFAR10()
+
+num_classes = 10
 
 model = Sequential()
+
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
