32b2fde2e582d230153ed24d0d3138887f76e0f9
Xsolute Lim <Xsolute01@gmail.com>
2018-11-06 17:24:20 +0900

    MOD: dataset 가져오는 함수 설정
    함수를 통해 dataset을 가져올 수 있도록 설정

--- dataset.py
+++ dataset.py
@@ -1,4 +1,6 @@
 # CIFAR-10 Small Image Classification
 from keras.datasets import cifar10
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+def loadCIFAR10():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    return (x_train, y_train), (x_test, y_test)
